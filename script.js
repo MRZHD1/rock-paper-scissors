d78f0fe98b9ede4bfb5d6b8d79a3c549eadbb27d
Omer <omerzahidali@gmail.com>
2022-10-01 10:20:42 +0300

    Adding the input and finishing the game

--- script.js
+++ script.js
@@ -37,3 +37,37 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
+function game() {
+    let score = 0
+    for (let i = 0; i < 5; i++) {
+        let play = prompt("Pick rock, paper, or scissors!").toLowerCase()
+        if (play != "rock" && play != "paper" && play != "scissors") {
+            alert("That was not a valid choice!")
+            i--
+        }
+        else {
+            let cpuPlay = String(getComputerChoice())
+            point = parseInt(playRound(play,cpuPlay))
+            if (point == -1) {
+                alert(`You lost by playing ${play} against ${cpuPlay}`)
+            }
+            else if (point == 0) {
+                alert(`Draw! Both players played ${play}`)
+            }
+            else {
+                alert(`Win! You played ${play} against ${cpuPlay}`)
+            }
+            score = score + point
+        }
+    }
+    if (score < 0) {
+        alert(`You lost! The CPU beat you ${Math.abs(point)} more time(s)`)
+    }
+    else if (score == 0) {
+        alert("Draw! What a conclusion!")
+    }
+    else {
+        alert(`A WIN! Congrats. You beat the CPU ${score} more time(s)`)
+    }
+}
+game()
